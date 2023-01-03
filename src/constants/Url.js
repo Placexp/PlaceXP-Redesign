e902582c4a03e8ca022d0cc65b83f38c72df0d1f
SabariGanesh-K <k.sabarii.ganesh@gmail.com>
2023-01-03 15:22:08 +0530

    backend production switch

--- src/constants/Url.js
+++ src/constants/Url.js
@@ -1,4 +1,4 @@
-export const Url = "https://placexp-server.onrender.com";
+export const Url = "https://placexp-server.up.railway.app";
 //testing:- https://placexp-server.onrender.com
 //production:- https://placexp-server.up.railway.app/
 //use testing while doing works in localhost, change it back to production while committing
